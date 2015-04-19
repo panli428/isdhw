032479cb151eed64cd38c4bf9fce5cc45ddc18e4
panli428 <1039859719@qq.com>
2015-04-19 18:58:33 +0800

    01

---
+++ main.py
@@ -0,0 +1,13 @@
+for i in range(1,10):
+        for j in range(i,10):
+            w=str.format("{0:1}*{1:1}={2:>2}",i,j,i*j)
+            print(w,end="  ")
+        print()
+for i in range(1,10):
+        for k in range(0,i-1):
+            print(end="        ")
+        for j in range(i,10):
+            m=str.format("{0:1}*{1:1}={2:>2}",i,j,i*j) 
+            print(m,end="  ")
+        print()
+
